06d4a70df531e0b542a9ff4e9d65381fcc1da6a6
v.tikhonov (notebook) <vladimir@tikhonov.by>
2015-09-06 16:04:36 +0300

    Add XYZ and Lab color classes

---
+++ Lib/LabColor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace Lib
+{
+    public class LabColor
+    {
+        public LabColor(XyzColor xyzSource)
+        {
+            BuildFromXyz(xyzSource.X, xyzSource.Y, xyzSource.Z);
+        }
+
+        public double L { get; private set; }
+        public double A { get; private set; }
+        public double B { get; private set; }
+
+        public static LabColor FromRgb(byte r, byte g, byte b)
+        {
+            return new LabColor(new XyzColor(Color.FromRgb(r, g, b)));
+        }
+
+        public override string ToString()
+        {
+            return $"L: {L}, A: {A}, B: {B}";
+        }
+
+        private void BuildFromXyz(double x, double y, double z)
+        {
+            L = 116.0*Fxyz(y/1.0) - 16;
+            A = 500.0*(Fxyz(x/0.9505) - Fxyz(y/1.0));
+            B = 200.0*(Fxyz(y/1.0) - Fxyz(z/1.0890));
+        }
+
+        private static double Fxyz(double t)
+        {
+            return ((t > 0.008856) ? Math.Pow(t, (1.0 / 3.0)) : (7.787 * t + 16.0 / 116.0));
+        }
+    }
+}
---
+++ Lib/XYZColor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace Lib
+{
+    public class XyzColor
+    {
+        public XyzColor(Color rgbSource)
+        {
+            BuildFromRgb(rgbSource.R, rgbSource.G, rgbSource.B);
+        }
+
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public double Z { get; private set; }
+
+        private void BuildFromRgb(int red, int green, int blue)
+        {
+            var rLinear = red / 255.0;
+            var gLinear = green / 255.0;
+            var bLinear = blue / 255.0;
+
+            var r = (rLinear > 0.04045) ? Math.Pow((rLinear + 0.055) / (
+                1 + 0.055), 2.2) : (rLinear / 12.92);
+            var g = (gLinear > 0.04045) ? Math.Pow((gLinear + 0.055) / (
+                1 + 0.055), 2.2) : (gLinear / 12.92);
+            var b = (bLinear > 0.04045) ? Math.Pow((bLinear + 0.055) / (
+                1 + 0.055), 2.2) : (bLinear / 12.92);
+
+            X = r*0.4124 + g*0.3576 + b*0.1805;
+            Y = r*0.2126 + g*0.7152 + b*0.0722;
+            Z = r*0.0193 + g*0.1192 + b*0.9505;
+        }
+    }
+}
